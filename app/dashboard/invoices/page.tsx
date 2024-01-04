93d4fde8cbdd9188601938f1558a577b6a03e9aa
Jon Wolff <jon.wolff@wolfftech.com>
2024-01-04 15:36:39 -0500

    Clean up invoice code

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,17 +1,14 @@
-import { fetchFilteredInvoices } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import InvoicesTable from "@/app/ui/invoices/table";
 
 export default async function Page() {
-
-
-    return (
-        <main>
-            <Breadcrumbs
-                breadcrumbs={[
-                    { label: 'Invoices', href: '/dashboard/customers', active: true },
-                ]} />
-            <InvoicesTable query="" currentPage={1} />
-        </main>
-    )
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/customers', active: true },
+        ]} />
+      <InvoicesTable query="" currentPage={1} />
+    </main>
+  )
 }
