7c10b40f855b54a1790a415f481edc308baeb2bc
Jon Wolff <jon.wolff@wolfftech.com>
2024-01-12 09:04:47 -0500

    'Theme' was causing some hydration error.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <Theme>
-        <body className={`${inter.className} antialiased`}>{children}</body>
-      </Theme>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
